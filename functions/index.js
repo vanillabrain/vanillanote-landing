eed014d25b3a07079a7bcc5acfacf650d397ddc1
yeongeunkwon <yeongeunkwon@vanillabrain.com>
2021-08-10 18:16:50 +0900

    added functions & hosting

---
+++ functions/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+//to make it work you need gmail account
+const gmailEmail = "vanillanoteapp@gmail.com"; //functions.config().gmail.login;
+const gmailPassword = "vanillanoteapp123";//functions.config().gmail.pass;
+
+admin.initializeApp();
+
+//creating function for sending emails
+var goMail = function (text) {
+
+//transporter is a way to send your emails
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: gmailEmail,
+            pass: gmailPassword
+        }
+    });
+
+    // setup email data with unicode symbols
+    //this is how your email are going to look like
+    const mailOptions = {
+        from: gmailEmail, // sender address
+        to: 'hello@vanillabrain.com', // list of receivers
+        subject: 'Vanilla Note App', // Subject line
+        text: text, // plain text body
+        html: text // html body
+    };
+
+    //this is callback function to return status to firebase console
+    const getDeliveryStatus = function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    };
+
+    //call of this function send an email, and return status
+    transporter.sendMail(mailOptions, getDeliveryStatus);
+};
+
+//.onDataAdded is watches for changes in database
+exports.onDataAdded = functions.database.ref('/queries/{sessionId}').onCreate(function (snap, context) {
+
+    //here we catch a new data, added to firebase database, it stored in a snap variable
+    const createdData = snap.val();
+    var text2 = createdData.mail;
+    var text1 = createdData.writerName;
+    var text3 = createdData.content;
+    var text = "Sender Name: " + text1
+        + "\n\n              Sender Email Address: "
+    + text2 + "\n\n               Content: " + text3;
+    //here we send new data using function for sending emails
+    goMail(text);
+});
---
+++ public/js/firebase.js
@@ -0,0 +1,51 @@
+'use strict';
+
+//grab a form
+const form = document.querySelector('.form');
+
+//grab inputs
+const inputEmail = form.querySelector('#writer_email');
+const writerName = form.querySelector('#writer_name');
+const content = form.querySelector('#form_content');
+
+//config your firebase push
+const config = {
+    apiKey: "AIzaSyAWs7ItBH2E7ATOyEsTEJThxBcYnjRt7wM",
+    authDomain: "vanillanoteapp.web.app",
+    databaseURL: "https://vanillanoteapp-default-rtdb.firebaseio.com/",
+    projectId: "vanillanoteapp",
+    storageBucket: "vanillanoteapp.appspot.com",
+    messagingSenderId: "443932673550"
+};
+
+
+//create a functions to push
+function firebasePush(email, writer_name, content) {
+
+
+    //prevents from braking
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+
+    //push itself
+    var mailsRef = firebase.database().ref('queries').push().set(
+        {
+            writerName: writer_name.value,
+            mail: email.value,
+            content: content.value
+        }
+    );
+
+}
+
+//push on form submit
+if (form) {
+    form.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        firebasePush(inputEmail, writerName, content);
+
+        //shows alert if everything went well.
+        return alert('Data Successfully Sent to Realtime Database');
+    })
+}
